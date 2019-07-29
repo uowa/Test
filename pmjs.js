c04238b63aebc68750c1a9ac9d4d6bfe2ab35789
Uowa <uowa905@yahoo.co.jp>
2019-07-30 01:24:51 +0900

    質問するのに邪魔な部分ある程度消した

--- pmjs.js
+++ pmjs.js
@@ -16,28 +16,28 @@ window.addEventListener('DOMContentLoaded', function() {
 
 
 
-
-
 //？？？？？？？？最終的にはクリックした位置に向かって、画像の向きを変えて動くアニメーションを作りたいんだけど、
 //とりあえず前段階でクリックした後に、距離に応じて斜めに画像が動く様にしたい。
 
 //クリックした位置にアバターの移動と画像切り替え
-//アバターの座標
+//アバターの元座標
 var aX = 0;
 var aY = 0;
+
+//微調整用
 var aW = 20;
 var aH = 50;
+
+//
 var moveX = 0;
-var moveY =0;
-const direction_src = new Array('img/upperRight.gif', 'img/lowerRight.gif', 'img/lowerLeft.gif','img/upperLeft.gif',
-                                'img/upper.gif',  'img/right.gif','img/lower.gif',  'img/left.gif');
-
-
+var moveY = 0;
 
 const avatarTimer = () =>{
+
 //？？？？　　　↓のaXを数字に変えたら一応動くから、ここが問題だと思うんだけど、どう直すのが正解なのかよくわからない。
 if(moveX < (mX-aX)){
-console.log(mX-aX);
+  console.log(mX-aX);
+
   moveX++;
   moveY++;
 
@@ -51,29 +51,5 @@ setTimeout("avatarTimer()",1);
 }
 
 document.getElementById('graphic').onclick = () => {
-  if (mX > aX+aW && mY < aY-aH) {
-      avatar.src = direction_src[0];
-  }
-  else if(mX > aX+aW && mY > aY){
-      avatar.src = direction_src[1];
-  }
-  else if(mX < aX-aW && mY > aY){
-      avatar.src = direction_src[2];
-  }
-  else if(mX < aX-aW && mY < aY-aH){
-      avatar.src = direction_src[3];
-  }
-  else if(mY < aY-aH){
-      avatar.src = direction_src[4];
-  }
-  else if(mX > aX+aW){
-      avatar.src = direction_src[5];
-  }
-  else if(mY > aY){
-      avatar.src = direction_src[6];
-  }
-  else if(mX < aX-aW){
-      avatar.src = direction_src[7];
-  }
 avatarTimer();
 }
